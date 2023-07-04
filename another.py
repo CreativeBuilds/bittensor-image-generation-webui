cba1373f00ec5c8d3a2d94293deda1fcf5bd7b58
CreativeBuilds <nick@creativebuilds.io>
2023-07-04 11:04:01 -0400

    removes unneeded py files

--- another.py
+++
@@ -1,8 +0,0 @@
-from inference import predict_pil, img_path
-from PIL import Image
-
-
-if __name__ == "__main__":
-    pil_image = Image.open(img_path)
-    prediction = predict_pil(pil_image)
-    print("Predicted aesthetic score:", prediction)
