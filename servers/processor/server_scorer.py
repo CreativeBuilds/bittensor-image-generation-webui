470ad435524579ebeca3ae3f84b8eb840d0749da
CreativeBuilds <nick@creativebuilds.io>
2023-08-07 14:40:28 -0400

    updating scoring model

--- servers/processor/server_scorer.py
+++ servers/processor/server_scorer.py
@@ -5,6 +5,8 @@ import base64
 import io
 from PIL import Image
 import bittensor as bt
+import torch
+from typing import List
 
 from waitress import serve
 
@@ -12,11 +14,42 @@ from ...inference import predict_pil
 from ..._utils._DEFAULTS import *
 from ..._utils.handle_outputs import add_prompt_to_file, GetPassedObj, GetBlockedObj
 
+import ImageReward as RM
+scoring_model = RM.load("ImageReward-v1.0")
+
 app = Flask(__name__, static_folder='build', static_url_path='/')
 
 CORS(app)
 
 active_users = {}
+
+def calculate_rewards_for_prompt_alignment(query, images: List[ Image.Image ]) -> (torch.FloatTensor, List[ Image.Image ]):
+
+    # Takes the original query and a list of responses, returns a tensor of rewards equal to the length of the responses.
+    with torch.no_grad():
+        ranking, scores = scoring_model.inference_rank(query.text, images)
+        # map ranking to top_images
+        top_images = [ images[i] for i in ranking ]
+        # sort scores best to worst
+        scores, _ = torch.sort( scores, descending = True )
+        # convert scores to array
+        scores = scores.numpy()
+
+    # if sum is 0 then return empty vector
+    if torch.sum( scores ) == 0:
+        return torch.zeros( len(scores), dtype = torch.float32 )
+
+    # preform exp on all values
+    init_scores = torch.exp( scores )
+
+    # set all values of 1 to 0
+    init_scores[init_scores == 1] = 0
+
+    # normalize the scores such that they sum to 1 but skip scores that are 0
+    init_scores = init_scores / torch.sum( init_scores )
+
+
+    return (init_scores, top_images)
 
 def create_app():
     app = Flask(__name__)
@@ -38,7 +71,7 @@ def create_app():
             if len(all_images) > 4:
 
                 # determine top 4 images using predict_pil
-                image_score_pair = ScoreImagesAesthetic(all_images)
+                image_score_pair = ScoreImagesAesthetic(all_images, prompt=request_body['text'])
                 percentage_blocked = GetPercentBlocked(image_score_pair)
 
 
@@ -100,16 +133,22 @@ def create_app():
         models = [pair[0]['model_type'] for pair in image_score_pair[:4]]
         return avg_image_scores,top_4_images_scores,top_4_image_seeds,resolution,image_hashes,parent_hashes,parent_hashes_dedupe,models
 
-    def ScoreImagesAesthetic(all_images):
+    def ScoreImagesAesthetic(all_images, prompt=""):
         image_score_pair = []
-        for image in all_images:
-            image_bytes = base64.b64decode(image['image'])
-            image_pil = Image.open(io.BytesIO(image_bytes))
-            score = predict_pil(image_pil)
-            image['aesthetic_score'] = score
-            print(f"score: {score}")
-            image_score_pair.append((image, score))
+        scores, images = calculate_rewards_for_prompt_alignment(prompt, all_images)
+        # zip images and scores together
+        image_score_pair = list(zip(images, scores))
+        # sort by score
+        image_score_pair.sort(key=lambda x: x[1], reverse=True)
         return image_score_pair
+        # for image in all_images:
+        #     image_bytes = base64.b64decode(image['image'])
+        #     image_pil = Image.open(io.BytesIO(image_bytes))
+        #     score = predict_pil(image_pil)
+        #     image['aesthetic_score'] = score
+        #     print(f"score: {score}")
+        #     image_score_pair.append((image, score))
+        # return image_score_pair
         
     return app
 
